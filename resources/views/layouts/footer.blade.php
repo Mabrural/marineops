d387b8090c75d4f0e7b84c6d007d9d02dc38f346
Mabrural <mabruralmutaqi@gmail.com>
2026-01-15 11:10:07 +0700

    templating di dashboard masih basic

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container-fluid d-flex justify-content-between">
+        <div class="copyright">
+            &copy; {{ now()->year }} All Right Reserved
+        </div>
+        <div>
+            Developed by <strong>Muhammad Mabrur Al Mutaqi</strong>
+        </div>
+    </div>
+</footer>
