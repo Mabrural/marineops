85c8a05b2291d4c27979d5eaed65efa405662ea3
Mabrural <mabruralmutaqi@gmail.com>
2026-01-15 17:40:24 +0700

    perubahan navbar dan sidebar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,8 +2,8 @@
     <div class="main-header-logo">
         <!-- Logo Header -->
         <div class="logo-header" data-background-color="dark">
-            <a href="index.html" class="logo">
-                <img src="{{ asset('assets/img/kaiadmin/logo_light.svg') }}" alt="navbar brand" class="navbar-brand"
+            <a href="/dashboard" class="logo">
+                <img src="{{ asset('assets/img/marineops/marine-ops-text-light.svg') }}" alt="navbar brand" class="navbar-brand"
                     height="20" />
             </a>
             <div class="nav-toggle">
