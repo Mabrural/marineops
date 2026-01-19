f4260e38e70bdb491246e4ae962c31e95011840e
Mabrural <mabruralmutaqi@gmail.com>
2026-01-19 16:42:55 +0700

    CRUD port done

--- resources/views/ports/edit.blade.php
+++ resources/views/ports/edit.blade.php
@@ -1,1 +1,76 @@
-ini edit port
+@extends('layouts.main')
+
+@section('container')
+    <div class="container">
+        <div class="page-inner">
+            <div class="page-header">
+                <h4 class="page-title">Edit Port</h4>
+                <ul class="breadcrumbs">
+                    <li class="nav-home">
+                        <a href="{{ route('dashboard') }}">
+                            <i class="flaticon-home"></i>
+                        </a>
+                    </li>
+                    <li class="separator">
+                        <i class="flaticon-right-arrow"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('ports.index') }}">Port</a>
+                    </li>
+                    <li class="separator">
+                        <i class="flaticon-right-arrow"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#">Edit Port</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-title">Port Information</div>
+                        </div>
+
+                        <form method="POST" action="{{ route('ports.update', $port) }}">
+                            @csrf
+                            @method('PUT')
+
+                            <div class="card-body">
+                                <div class="row">
+
+                                    <!-- Port Name -->
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="name">Port Name <span class="text-danger">*</span></label>
+                                            <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                                id="name" name="name" value="{{ old('name', $port->name) }}"
+                                                placeholder="e.g. PT Global Maritim Nusantara" required>
+
+                                            @error('name')
+                                                <span class="invalid-feedback">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                            <div class="card-action">
+                                <button type="submit" class="btn btn-primary">
+                                    Update Port
+                                </button>
+                                <a href="{{ route('ports.index') }}" class="btn btn-danger">
+                                    Cancel
+                                </a>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
